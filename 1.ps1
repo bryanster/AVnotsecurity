31c950c8c906ba7793424d9c6d5e03fa4f7d15bc
bryanster <45668775+bryanster@users.noreply.github.com>
2022-07-29 17:53:15 +0200

    Update 1.ps1

--- 1.ps1
+++ 1.ps1
@@ -3,7 +3,7 @@ if ($connectTestResult.TcpTestSucceeded) {
     # Het wachtwoord opslaan, zodat de schijf blijft behouden bij opnieuw opstarten
     cmd.exe /C "cmdkey /add:`"avnotsecurity.file.core.windows.net`" /user:`"localhost\avnotsecurity`" /pass:`"DrRpr44IoNV9tLKBNPjoF7hAuCAKnwlQNNBYB41M8Thj50tcdJLp8OxTxzdgH/F0iL/09WjvJ1RF+AStPMdl2w==`""
     # Het station koppelen
-    New-PSDrive -Name Z -PSProvider FileSystem -Root "\\avnotsecurity.file.core.windows.net\avnotsec" -Persist
+    New-PSDrive -Name Z -PSProvider FileSystem -Root "\\avnotsecurity.file.core.windows.net\avnotsecurity" -Persist
 } else {
     Write-Error -Message "Unable to reach the Azure storage account via port 445. Check to make sure your organization or ISP is not blocking port 445, or use Azure P2S VPN, Azure S2S VPN, or Express Route to tunnel SMB traffic over a different port."
 }
